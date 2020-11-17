9945fb34cda7858be9e997601a19a5eec65cf016
maryanne-njeri <maryanjeryrose@gmail.com>
2020-11-17 14:06:57 +0300

    First commit

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
+// header behaviour
+
+// selecting objects
 var selectObject = {
 	menuItems : document.getElementById('menuItems'),
 	bars : document.getElementById('bars')
 }
-
+// manipulation
 function returnFunc() {
 	if(selectObject.menuItems.style.display === 'none'){	
 		selectObject.menuItems.style.display = 'block';
 		selectObject.menuItems.style.transition = 'width 2s linear 1s';
 		selectObject.bars.innerHTML =  '<i class="fa fa-times"></i>';
-		// selectObject.bars.style.fontSize = '50px';
 	}else{
 		selectObject.menuItems.style.display = 'none';
 		selectObject.bars.innerHTML = '<i class="fa fa-bars"></i>';
 	}
 }
-
+// calling function
 selectObject.bars.addEventListener('click', returnFunc);
+
+
+// blog behaviour
+
+var selectObjectTwo = {
+	blogOne : document.querySelector('.blogOne'),
+	blogTwo : document.querySelector('.blogTwo'),
+	blogThree : document.querySelector('.blogOne')
+}
+
+function returnFuncTwo() {
+	if () {}
+	selectObjectTwo.blogOne.innerHTML = 'Read More...';
+	selectObjectTwo.blogOne.style.backgroundColor = 'blue';
+}
+
+selectObjectTwo.blogOne.addEventListener('mouseover', returnFuncTwo);
